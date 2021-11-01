5be9689058d8f5cf3a557d722b438372bd4bde51
Ronan Loughlin <loughlinronan@gmail.com>
2021-11-01 08:25:15 -0400

    fixed build issues

--- src/components/a6/Build/NavigationSidebar/index.js
+++ src/components/a6/Build/NavigationSidebar/index.js
@@ -7,23 +7,23 @@ const NavigationSidebar = (
     return(
         <>
             <div className="list-group">
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fab fa-twitter"></i></a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-home"></i> Home</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-hashtag"></i> Explore</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-bell"></i> Notifications</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-envelope"></i> Messages</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-bookmark"></i> Bookmarks</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-list"></i> Lists</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-user-alt"></i> Profile</a>
-                <a className={'list-group-item ${active === "home"?"active":"}'} href="/">
+                <a className="list-group-item" href="/">
                     <i className="fas fa-comment-dots"></i> More</a>
             </div>
             <div className="d-grid mt-2">
