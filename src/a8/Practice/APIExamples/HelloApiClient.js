0e224e9ce97adce46d94577eb192b4340f02c15b
Ronan Loughlin <loughlinronan@gmail.com>
2021-11-21 19:05:44 -0500

    A8 Node fa21

--- src/a8/Practice/APIExamples/HelloApiClient.js
+++ src/a8/Practice/APIExamples/HelloApiClient.js
@@ -3,7 +3,7 @@ import React, {useEffect, useState} from "react";
 const HelloApiClient = () => {
     const [hello, setHello] = useState('');
     useEffect(() => {
-        fetch('http://localhost:4000/hello')
+        fetch('http://web-dev-cs4550.herokuapp.com/hello')
             .then(response => response.text())
             .then(text => setHello(text));
     }, []);
